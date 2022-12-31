97fffa4e384f8a20bf9cd01b9eaffc96f66f009d
Gabriel Ortega <72330120+GabsOrtega@users.noreply.github.com>
2022-12-30 21:03:27 -0300

    Add files via upload

--- php/connect.php
+++ php/connect.php
@@ -2,10 +2,10 @@
 
 date_default_timezone_set("America/Sao_Paulo");
 
-$username = 'epiz_33288903';
-$password = 'DfnklR5xRcV';
+$username = 'root';
+$password = '';
 
-$connect = new PDO('mysql:host=sql310.epizy.com;dbname=epiz_33288903_vnom;charset=utf8', $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+$connect = new PDO('mysql:host=localhost;dbname=vnom;charset=utf8', $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 ?>
